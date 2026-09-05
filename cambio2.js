cb434ac4f79b5aa2317d908da3984d8b48c428b2
dinalbe <alberti.dina@gmail.com>
2026-09-05 17:24:17 +0200

    Add files via upload

---
+++ cambio2.js
@@ -0,0 +1,20 @@
+// Contenitore delle funzioni per i bottoni di Progetto CAMBIO
+window.eseguiFase1 = function() {
+    // Inserisci qui il codice della Fase 1
+    console.log("Eseguita Fase 1");
+};
+
+window.eseguiFase2 = function() {
+    // Inserisci qui il codice della Fase 2
+    console.log("Eseguita Fase 2");
+};
+
+window.eseguiFase3 = function() {
+    // Inserisci qui il codice della Fase 3
+    console.log("Eseguita Fase 3");
+};
+
+window.eseguiFase4 = function() {
+    // Inserisci qui il codice della Fase 4
+    console.log("Eseguita Fase 4");
+};
